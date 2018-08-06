fdd3f9016862b7a0886358dc08106ffc41d6fd31
Hemavathy <hemavathydhamodharan97@gmail.com>
2018-08-06 09:55:28 +0530

    Pe-2

---
+++ pe2/src/main/java/com/stackroute/pe2/App.java
@@ -0,0 +1,146 @@
+package com.stackroute.pe2;
+public class App 
+{
+	public boolean reverse(int num) {
+			int rem,temp,sum=0;
+				temp=num;
+				while(num>0)
+				{
+				rem=num%10;
+				sum=(sum*10)+rem;
+				num=num/10;
+				}
+				if(temp==sum)
+				{
+					return true;
+				}
+				else
+				{
+					return false;
+				}
+			}
+	
+	
+	
+	public boolean power(int num) {
+		if(num==0)
+			return false;
+		while(num!=1)
+		{
+			if(num%4!=0)
+				return false;
+			num=num/4;
+		}
+		return true;
+		}
+
+	
+	
+	
+	public boolean even(int num) {
+			if(num%2==0)
+				return true;
+			else
+		return false;
+		}
+	
+	
+	
+	
+	public String intfact(int num)
+	{
+		int i,fact=1;
+		for(i=1;i<=num;i++) {
+			fact=fact*i;
+		}
+		
+		String s= "The factorial of "+num+" is "+fact;
+		
+		
+		
+		return s;
+	}
+	public String longFactorial(long num)
+	{
+		int i;
+		long fact=1;
+		for(i=1;i<=num;i++) {
+			fact=fact*i;
+		}
+		
+		String s= "The factorial of "+num+" is "+fact;
+		
+		
+		
+		return s;	
+	}
+	
+		 
+	     	
+	
+	
+
+		String name=null;
+		int age;
+		float salary;
+		public void member(String name, int age, float salary)
+		{
+			this.name=name;
+			this.age=age;
+			this.salary=salary;
+		}
+		public String membervariable()
+		{
+			return name+ +age+ +salary;
+		}		
+
+		
+		
+		
+
+		public String average(int num[])
+		{
+			int x=num.length,y=0;
+			float avg;
+			for(int i=0;i<x;i++) {
+				y=y+num[i];
+			}
+			
+			avg=y/x;
+			avg=(float) (avg+0.50);
+			String s= "The average is "+avg+0;
+			return s;
+		}
+		public String min(int num[])
+		{
+			int min=num[0];
+			int x=num.length;
+			for(int i=1;i<x;i++)
+			{
+				if(num[i]<min)
+				{
+					min=num[i];
+				}
+			}
+			String s= "The minimum is "+min;
+			return s;
+		}
+		public String max(int num[])
+		{
+			int max=num[0];
+			
+			for(int i=1;i<num.length;i++)
+			{
+				if(num[i]>max)
+				{
+					max=num[i];
+				}
+			}
+			String s= "The maximum is "+max;
+			return s;
+		}
+
+	}
+
+
+
---
+++ pe2/src/test/java/com/stackroute/pe2/AppTest.java
@@ -0,0 +1,112 @@
+package com.stackroute.pe2;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertNotEquals;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class AppTest {
+    private static App pal;
+
+    @BeforeClass
+    public static void setup() {
+        pal = new App();
+
+    }
+
+    @AfterClass
+    public static void teardown() {
+        pal = null;
+
+    }
+
+    //@SuppressWarnings("deprecation")
+    @Test
+    public void testCheckPalindrome() {
+        int num = 24542;
+        assertEquals(true, pal.reverse(num));
+        assertNotNull("failure",pal.reverse(num));
+		assertNotEquals(1,pal.reverse(num));
+    }	
+    
+  //@SuppressWarnings("deprecation")
+    @Test
+    public void testCheckPower() {
+        int num = 64;
+        assertEquals(true, pal.power(num));
+        assertNotNull("failure",pal.power(num));
+		assertNotEquals(1,pal.power(num));
+				
+	}
+    
+    @Test
+	public void test11()
+	{
+		assertEquals("The factorial of 12 is 479001600", pal.intfact(12));
+	}
+	@Test
+	public void test12()
+	{
+		assertEquals("The factorial of 2 is 2", pal.intfact(2));
+	}
+	@Test
+	public void test13()
+	{
+		assertEquals("The factorial of 20 is 2432902008176640000", pal.longFactorial(20));
+	}
+	@Test
+	public void test14()
+	{
+		assertNotEquals("10",pal.longFactorial(10));
+	}
+
+    
+    @Test
+    public void testCheckEven() {
+    	int num = 16;
+    	assertEquals(true, pal.even(num));
+    	assertNotNull("failure",pal.even(num));
+    	assertNotEquals(1,pal.even(num));
+	}
+    
+   
+    
+    
+    int[] n= {86,65,98,77};
+	@Test
+	public void test1()
+	{
+		assertEquals("The average is 81.50", pal.average(n));
+	}
+	@Test
+	public void test2()
+	{
+		assertEquals("The minimum is 65", pal.min(n));
+	}
+	@Test
+	public void test3()
+	{
+		assertEquals("The maximum is 98", pal.max(n));
+	}
+	@Test
+	public void test4()
+	{
+		assertNotEquals("The minimum is 86",pal.min(n));
+	}
+
+	
+	
+	 @Test
+	    public void testCheckMember()
+	    {
+	    	String s=pal.membervariable();
+	        assertEquals("Harry Potter 30 2500.3",s);
+	        assertNotEquals("failure",s);
+			}
+	    
+}
+    	   
